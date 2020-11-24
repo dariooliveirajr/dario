de3b612bd94dc4e7d411d8af944d5818110c7357
Dário Oliveira Júnior <juniordario06@gmail.com>
2020-11-24 12:11:22 -0300

    Habilidades

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ module.exports = function(app){
         }];
         var habilidades = [{
         "logo":"web.png",
-        "texto":"Desenvolvimento de Fron-end utilizando de HTML5, CSS3 e JAVASCRIPT, incluindo bibliotecas e frameworks como Bootstrap, JQuery, e EJS.",
+        "texto":"Desenvolvimento de Front-end utilizando de HTML5, CSS3 e JAVASCRIPT, incluindo bibliotecas e frameworks como Bootstrap, JQuery, React e EJS.",
             "texto2":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"   
         },
       {
